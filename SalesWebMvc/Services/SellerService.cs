367eccf10f33a373e2c1d90a14d73dfe7e771c1d
Rafaela Ranieri Lassen <lassenrafaela@gmail.com>
2024-11-12 14:13:10 -0300

    SelerService and basic FindA11

---
+++ SalesWebMvc/Services/SellerService.cs
@@ -0,0 +1,23 @@
+﻿using SalesWebMvc.Data;
+using SalesWebMvc.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMvc.Services
+{
+    public class SellerService
+    {
+      
+        private readonly SalesWebMvcContext _context;
+
+        public SellerService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindA11()
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
