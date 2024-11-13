4db40efa19a808b2d51ffabcd76086316979f295
Rafaela Ranieri Lassen <lassenrafaela@gmail.com>
2024-11-13 17:35:09 -0300

    tela tetalhes e eager loading

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -1,4 +1,5 @@
-﻿using SalesWebMvc.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Data;
 using SalesWebMvc.Models;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,14 +23,13 @@ namespace SalesWebMvc.Services
 
         public void Insert(Seller obj)
         {
-          
             _context.Add(obj);
             _context.SaveChanges();
         }
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
 
         public void Remove (int id)
